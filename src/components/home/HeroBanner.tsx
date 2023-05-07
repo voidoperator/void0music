d9c08e859e32755e5a69004e8d68f37301452084
voidoperator <julionunez@me.com>
2023-05-06 20:00:02 -0400

    feat: navbar styles and update herobanner placeholder

--- src/components/home/HeroBanner.tsx
+++ src/components/home/HeroBanner.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-// import { useStaticQuery, graphql } from 'gatsby'
-
-const heroData = {
-  headline: 'none',
-  title: 'none',
-  heroImage: {
-    url: 'none',
-  },
-};
 
 const HeroBanner = () => {
   return (
-    <section
-      className='relative bg-cover bg-center bg-no-repeat h-[300px] md:h-[450px] lg:h-[615px]'
-      style={{ backgroundImage: `url(${heroData?.heroImage.url})` }}
-    >
+    <section className='relative bg-slate-600 border-2 border-white bg-cover bg-center bg-no-repeat h-[100svh]'>
       <div className='bottom-10 lg:bottom-20 absolute left-0 w-full'>
         <div className='container'>
           <h1 className='lg:text-3xl lg:mb-7 mb-4 text-2xl font-bold text-white uppercase whitespace-pre-line'>
-            {heroData?.title}
+            {'herobanner placeholder'}
           </h1>
-          <p className='lg:text-xl text-base font-bold text-white uppercase'>{heroData?.headline}</p>
+          <p className='lg:text-xl text-base font-bold text-white uppercase'>{'herobanner placeholder'}</p>
         </div>
       </div>
     </section>
--- src/components/home/NavBar.tsx
+++ src/components/home/NavBar.tsx
@@ -1,34 +1,94 @@
-import React, { ReactNode } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import tw from 'tailwind-styled-components';
+import { motion, useAnimation } from 'framer-motion';
 import { Link } from 'gatsby';
+import { isBrowser } from '../../utility/utility';
 import { VzLogoIcon } from '../icons/VzLogoIcon';
 import SocialIcon from '../icons/SocialIcon';
 import type { NavBarProps } from '../../types/contentful';
 
+const Header: Motion.Tag<'header'> = tw(motion.header)`
+  fixed z-20 w-[100svw]
+`;
+const Nav = tw.nav`
+  py-8 bg-transparent
+`;
+const Container = tw.div`
+  flex items-center justify-between px-10
+`;
+const SocialIconsWrapper = tw.div`
+  flex flex-row gap-6
+`;
+const IconsWrapper = tw.div`
+  flex items-center space-x-6
+`;
+const IconAnchor = tw.a`z-20
+  h-full w-full
+`;
+const Gradient: Motion.Tag<'div'> = tw(motion.div)`
+  w-screen h-[16svh] absolute top-0 left-0 z-10
+  bg-gradient-to-t from-transparent via-black/25 to-black/50
+`;
+
 const NavBar = ({ navItems }: NavBarProps) => {
+  const [scroll, setScroll] = useState<number>(0);
+  const headerRef = useRef<HTMLDivElement>(null);
+  const gradientControls = useAnimation();
+  const headerControls = useAnimation();
+
+  useEffect(() => {
+    if (!isBrowser) return;
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const handleScroll = () => {
+    if (!isBrowser) return;
+    const yAxis = window.pageYOffset;
+    if (yAxis === 0) {
+      headerControls.start({ top: '0rem' });
+      gradientControls.start({ opacity: 0 });
+    } else {
+      headerControls.start({ top: '-1.25rem' });
+      gradientControls.start({ opacity: 1 });
+    }
+    setScroll(yAxis);
+  };
+
   const vzLogo = navItems[0];
+
   return (
-    <header>
-      <nav className='py-4 bg-transparent'>
-        <div className='container flex items-center justify-between'>
-          <Link className='text-lg' to='/linktree' title={vzLogo.title}>
-            <VzLogoIcon twClasses='h-24 text-white' />
+    <Header initial={{ top: '0rem' }} animate={headerControls} transition={{ duration: 1, ease: 'easeInOut' }}>
+      <Nav>
+        <Container>
+          <Link className='text-lg z-20' to='/linktree' title={vzLogo.title}>
+            <VzLogoIcon twClasses='h-24' />
           </Link>
-          <div className='flex flex-row gap-6'>
-            <ul className='flex items-center text-white'>
+          <SocialIconsWrapper>
+            <IconsWrapper>
               {navItems.map((item, index) => {
                 if (index === 0) return;
                 const { id, title, hyperlink } = item;
                 return (
-                  <a key={id} target='_blank' href={hyperlink} title={title}>
-                    <SocialIcon icon={title} twClasses='h-9 p-1' />
-                  </a>
+                  <IconAnchor key={id} target='_blank' href={hyperlink} title={title}>
+                    <SocialIcon icon={title} twClasses='h-7' />
+                  </IconAnchor>
                 );
               })}
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
+            </IconsWrapper>
+          </SocialIconsWrapper>
+        </Container>
+      </Nav>
+      <Gradient
+        ref={headerRef}
+        initial={{ opacity: 0 }}
+        animate={gradientControls}
+        transition={{ duration: 1, ease: 'easeInOut' }}
+      />
+    </Header>
   );
 };
 
